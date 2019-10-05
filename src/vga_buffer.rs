62b410dd523c0f29bf983bb9f47dd88ecd7e90d8
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-10-05 08:39:54 -0700

    Add write_string and write! functionality

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,6 @@
+use volatile::Volatile;
+use core::fmt;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -42,7 +45,7 @@ const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 pub struct Writer {
@@ -63,15 +66,46 @@ impl Writer {
                 let row = BUFFER_HEIGHT - 1;
                 let col = self.column_position;
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code,
-                };
+                });
                 self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, string: &str) {
+        for byte in string.bytes() {
+            match byte {
+                // Only supports ASCII and the additional bytes of code page 437
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // Print '■' for unsupported characters
+                _ => self.write_byte(0xfe)
             }
         }
     }
 
     // TODO: Write newline or shift buffer up
     fn new_line(&mut self) {}
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+pub fn print_something() {
+    use fmt::Write;
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Green, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_byte(b'H');
+    writer.write_string("ello! ");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
