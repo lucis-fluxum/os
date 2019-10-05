093eceb1b228c8c4411d5c7fed02f5e3559d113e
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-10-05 10:39:28 -0700

    Panic handler now prints panic info

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,15 @@ use core::panic::PanicInfo;
 mod io;
 
 #[panic_handler]
-fn panic(_inf: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Here is some text!");
-    print!("And some more text.");
+    println!("And some more text.");
+    panic!("Panic message");
     loop {}
 }
